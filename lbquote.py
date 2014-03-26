050e0905fa5e8d3ce74b0ba80e679db331fae720
bV <bv@bvector.net>
2014-03-26 19:47:53 +0000

    Install requests module (pip install requests) Import requests module and add code to fetch and display prices

--- lbquote.py
+++ lbquote.py
@@ -1,9 +1,13 @@
 from flask import Flask
+import requests
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    price = requests.get('https://coinbase.com/api/v1/prices/buy')
+    return str(price.text)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
