dd2b5cebda42212ca77085563322ac2dc198bc45
bV <bv@bvector.net>
2014-03-26 20:14:34 +0000

    Change to return only the total price

--- lbquote.py
+++ lbquote.py
@@ -2,12 +2,12 @@ from flask import Flask
 import requests
 
 app = Flask(__name__)
-
+app.debug = True
 
 @app.route('/')
 def hello_world():
-    price = requests.get('https://coinbase.com/api/v1/prices/buy')
-    return str(price.text)
+    price = requests.get('https://coinbase.com/api/v1/prices/buy').json()
+    return str(price['total']['amount'])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
