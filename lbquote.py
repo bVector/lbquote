b3b86591dc11d978282017b7ed444e614487324e
bV <bv@bvector.net>
2014-03-26 20:32:43 +0000

    Add markup Convert price to int and multiply by markup

--- lbquote.py
+++ lbquote.py
@@ -2,12 +2,16 @@ from flask import Flask
 import requests
 
 app = Flask(__name__)
-app.debug = True
+#app.debug = True
+
+markup = 1.07
 
 @app.route('/')
 def hello_world():
-    price = requests.get('https://coinbase.com/api/v1/prices/buy').json()
-    return str(price['total']['amount'])
+    price = requests.get('https://coinbase.com/api/v1/prices/buy').json()['total']['amount']
+    price = int(float(price))
+    price = int(markup * price)
+    return str(price)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
